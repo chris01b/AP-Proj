e6ee93e96dc3dae754ea67f038d817ab567047a7
chris01b <nova6ers@gmail.com>
2017-03-30 22:24:12 -0400

    Removed test

--- getData.js
+++ getData.js
@@ -2,19 +2,21 @@ var request = require('request');
 
 const baseURL = 'https://maps.googleapis.com/maps/api/elevation/json';
 
-module.exports.getElevation = function(lat, long, APIkey, callback) {
-    var requestURL = baseURL + '?locations=' + lat + ',' + long + '&key=' + APIkey;
-    
-    request(requestURL, function (err, response, body) {
-        var pbody = JSON.parse(body);
+module.exports = {
+    getElevation: function(lat, long, APIkey, callback) {
         
-    	if (err) console.error('request error:', err);
-    	
-    	else if (pbody.status !== 'OK') {
-    	    callback(pbody.status + ':' + pbody.error_message, null);
-    	}
-    	else {
-    	    callback(null, pbody.results[0].elevation);
-    	}
-    });
-};
+        var requestURL = baseURL + '?locations=' + lat + ',' + long + '&key=' + APIkey;
+        
+        request(requestURL, function (err, response, body) {
+            var pbody = JSON.parse(body);
+            
+            if (err) console.error('request error:', err);
+        	
+        	else if (pbody.status !== 'OK') {
+        	    callback(pbody.status + ':' + pbody.error_message, null);
+        	} else {
+        	    callback(null, pbody.results[0].elevation);
+        	}
+        });
+    }
+}
--- test.js
+++
@@ -1,8 +0,0 @@
-var getData = require('./getData');
-
-const APIkey = 'AIzaSyDJUarszRbkOE-7jmdWmU9SKEKduoLFxYY';
-
-getData.getElevation(39.7391536, -104.9847034, APIkey, function(err, elevation) {
-    if (err) console.error(err);
-    console.log(elevation);
-});
