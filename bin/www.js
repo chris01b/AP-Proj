b7418a745a5b1b2d16dd0ba19be28f71565b0c7b
chris01b <nova6ers@gmail.com>
2017-04-01 16:35:03 -0400

    Changed fonts and indents

--- bin/www.js
+++ bin/www.js
@@ -47,19 +47,19 @@ server.on('listening', onListening);
  */
 
 function normalizePort(val) {
-  var port = parseInt(val, 10);
+    var port = parseInt(val, 10);
 
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
 
-  if (port >= 0) {
-    // port number
-    return port;
-  }
+    if (port >= 0) {
+        // port number
+        return port;
+    }
 
-  return false;
+    return false;
 }
 
 /**
@@ -67,27 +67,27 @@ function normalizePort(val) {
  */
 
 function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
 
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
 
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
+    // handle specific listen errors with friendly messages
+switch (error.code) {
     case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
     case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
     default:
-      throw error;
-  }
+        throw error;
+    }
 }
 
 /**
@@ -95,11 +95,11 @@ function onError(error) {
  */
 
 function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
 }
 
 /**
@@ -107,13 +107,13 @@ function onListening() {
  */
 
 io.on('connection', function (socket) {
-  socket.emit('sendpls', function(){});
-  socket.on('coords', function (data) {
-    // At this point, we will have executed getData.js on the location and then return it
-    getData.getElevation(data.lat, data.lng, APIkey, function(err, elevation) {
-        if (err) console.error(err);
-        console.log(elevation);
-        socket.emit('elevation', elevation);
+    console.log('A user connected');
+    socket.on('coords', function (data) {
+        // At this point, we will execute getData.js on the location and then return it
+        getData.getElevation(data.lat, data.lng, APIkey, function(err, elevation) {
+            if (err) console.error(err);
+            console.log(elevation);
+            socket.emit('elevation', elevation);
+        });
     });
-  });
 });
--- public/javascripts/location.js
+++ public/javascripts/location.js
@@ -2,27 +2,34 @@
  * TAKEN FROM https://developers.google.com/maps/documentation/javascript/geolocation
  */
 
+/*eslint-env jquery */
+/*globals io */
 var socket = io.connect(location.origin);
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var pos = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    };
-    
-    // We want to send pos
-    socket.on('sendpls', function() {
-      socket.emit('coords', pos);
-    });
-    socket.on('elevation', function(elevation) {
-        $(document).ready(function() {
-          $('#elevationID').text(elevation);
+
+function doLocation() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+        console.log(pos); // CURRENTLY NOT SENDING!!! PLS FIX!
+        
+        // send pos
+        socket.emit('coords', pos);
+        
+        socket.on('elevation', function(elevation) {
+            $(document).ready(function() {
+                $('#elevationID').text(elevation + ' meters');
+            });
         });
+        
+    }, function(err) {
+          console.error(err);
     });
-    
-  }, function(err) {
-      console.error(err);
-  });
+}
+
+if (navigator.geolocation) {
+    setInterval(doLocation, 2000);
 } else {
   console.log('Browser doesn\'t support Geolocation'); // Browser doesn't support Geolocation
 }
