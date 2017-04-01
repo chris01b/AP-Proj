d20873cdb3d8c8e140e577323ba010abf6353c4c
chris01b <nova6ers@gmail.com>
2017-04-01 16:40:44 -0400

    Fixed getData.js formatting

--- getData.js
+++ getData.js
@@ -4,14 +4,10 @@ const baseURL = 'https://maps.googleapis.com/maps/api/elevation/json';
 
 module.exports = {
     getElevation: function(lat, long, APIkey, callback) {
-        
         var requestURL = baseURL + '?locations=' + lat + ',' + long + '&key=' + APIkey;
-        
         request(requestURL, function (err, response, body) {
             var pbody = JSON.parse(body);
-            
             if (err) console.error('request error:', err);
-        	
         	else if (pbody.status !== 'OK') {
         	    callback(pbody.status + ':' + pbody.error_message, null);
         	} else {
@@ -19,4 +15,4 @@ module.exports = {
         	}
         });
     }
-}
+};
