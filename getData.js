683e25fc083c25c7785fb7d4dc3f974ba1a486a4
chris01b <nova6ers@gmail.com>
2017-04-02 17:28:55 -0400

    Added preliminary speedLimit functionality

--- getData.js
+++ getData.js
@@ -1,18 +1,30 @@
 var request = require('request');
 
-const baseURL = 'https://maps.googleapis.com/maps/api/elevation/json';
+const baseElevationURL = 'https://maps.googleapis.com/maps/api/elevation/json';
+const baseRoadsURL = 'https://roads.googleapis.com/v1/speedLimits';
 
 module.exports = {
     getElevation: function(lat, long, APIkey, callback) {
-        var requestURL = baseURL + '?locations=' + lat + ',' + long + '&key=' + APIkey;
+        var requestURL = baseElevationURL + '?locations=' + lat + ',' + long + '&key=' + APIkey;
         request(requestURL, function (err, response, body) {
             var pbody = JSON.parse(body);
             if (err) console.error('request error:', err);
         	else if (pbody.status !== 'OK') {
-        	    callback(pbody.status + ':' + pbody.error_message, null);
-        	} else {
-        	    callback(null, pbody.results[0].elevation);
-        	}
+                callback(pbody.status + ':' + pbody.error_message, null);
+            } else {
+                callback(null, pbody.results[0].elevation);
+            }
+        });
+    },
+
+    getSpeedLimit: function(path, APIkey) {
+        var requestURL = baseURL + '?path=' + lat + ',' + long + '&key=' + APIkey;
+        request(requestURL, function (err, response, body) {
+            var pbody = JSON.parse(body);
+            if (err) console.error('request error:', err);
+            else {
+                callback('warning' + ':' + pbody.warningMessage, pbody.speedLimits[0].speedLimit);
+            }   
         });
     }
 };
